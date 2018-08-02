eb481ed5c2eed83f06a5513659a8f127fbbed91a
bdemers1055 <bdemers1055@gmail.com>
2018-08-02 19:44:37 -0400

    stubbed out basic routes for CRUD

--- index.js
+++ index.js
@@ -9,6 +9,29 @@ const port = process.env.PORT || 8008;
 
 
 // routes
+server.get('/pets', (req, res) => {
+    res.send('getting all pet');
+});
+
+// get one special et by id
+server.get('/pets/:id', (req, res) => {
+    res.send(`getting ${req.params.id} pet`);
+});
+
+// create new pet
+server.post('/pets', (req, res) => {
+    res.send('new pet');
+});
+
+//update pet by id
+server.put('/pets/:id', (req, res) => {
+    res.send(`updating ${req.params.id} pet`);
+});
+
+//delete pet by id
+server.delete('/pets/:id', (req, res) => {
+    res.send(`deleting ${req.params.id} pet`);
+});
 
 // kick it off
 
