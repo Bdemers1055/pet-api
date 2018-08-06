9a34b4172589fb55209d09f66de1188acbf0f836
bdemers1055 <bdemers1055@gmail.com>
2018-08-06 19:10:05 -0400

    added additional fields and validation to pet model

--- models/pet.js
+++ models/pet.js
@@ -1,5 +1,35 @@
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema; // Schema class 
 
-const Pet = mongoose.model('Pet', { name: String, owner: String });
+const petSchema = new Schema({ // "petSchema" is variable name
+    name: {
+        type: String,
+        required: true,
+    },
+    owner: {
+        type: String,
+        required: true,
+    },
+    petType: {
+        type: String,
+        enum: ["cat", "dog"],
+        lowercase: true,
+        trim: true,
+        required: true
+    },
+    age: {
+        type: Number,
+        min: 0,
+        max: 30,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        required: true,
+        default: Date.now
+    }
+});
+
+const Pet = mongoose.model('Pet', petSchema);
 
 module.exports = Pet;
