9c6b3578afdea24ac501c1e21c9f06766422441a
bdemers1055 <bdemers1055@gmail.com>
2018-08-06 19:38:26 -0400

    extracted routes to pet router

--- index.js
+++ index.js
@@ -15,6 +15,8 @@ mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true});
 // setup our port
 const port = process.env.PORT || 8008;
 
+//routers
+const petRouter = require('./routers/pets');
 
 // power ups/middleware
 server.use(helmet());
@@ -22,91 +24,8 @@ server.use(morgan("combined")); // status logging
 server.use(bodyParser.json()); // accept json data
 server.use(bodyParser.urlencoded({ extended: true })); // accept html form data
 
-//models
-const Pet = require('./models/pet');
-
-// routes
-//get all pets
-server.get('/pets', async (req, res) => {
-    try {
-        const pets = await Pet.find();
-        res.status(200).json({
-            pets: pets
-        });
-    } 
-    catch(err) {
-        res.status(500).json({
-            msg: 'broken'
-        });
-    }
-});
-
-// get one special et by id
-server.get('/pets/:id', async (req, res) => {
-    const { id } = req.params; 
-    try {
-        const pets = await Pet.find({ _id: id });
-        res.status(200).json({
-            pets: pets
-        });
-    }
-    catch(err) {
-        res.status(500).json({
-            msg: 'broken'
-        });
-    }
-});
-
-// create new pet
-server.post('/pets/', async (req, res) => {
-    const { name, owner, petType, age, createdAt } = req.body;
-    try {
-        const pet = new Pet({ name, owner, petType, age, createdAt });
-        await pet.save();
-        res.status(201).json({
-            msg: 'saved pet',
-            pet
-        });
-    } catch(err){
-        res.status(500).json({
-            msg: 'pet not created'
-        });
-    }
-});
-
-//update pet by id
-server.put('/pets/:id', async (req, res) => {
-    const { id } = req.params;
-    const { name, owner } = req.body;
-    try {
-        const updatedPet = await Pet.findByIdAndUpdate( id, { name, owner }, { new: true });
-        res.status(200).json({
-            msg: 'update successful',
-            pet: updatedPet
-        });
-    } catch (error) {
-        res.status(500).json({
-            msg: 'pet did not update'
-        });
-    }
-});
-
-//delete pet by id
-server.delete('/pets/:id', async (req, res) => {
-    const { id } = req.params; 
-    try {
-        await Pet.findByIdAndRemove(id);
-        res.status(200).json({
-            msg: 'yay deleted!'
-        });
-    }
-    catch(err) {
-            res.status(500).json({
-                msg: 'broken'
-            });
-    }
-});
-
+//routes
+server.use(petRouter);
 
 // kick it off
 
---
+++ routers/pets.js
@@ -0,0 +1,88 @@
+const express = require('express');
+const router = express.Router(); // tiny lego brick
+const Pet = require('../models/pet');
+
+// routes
+//get all pets
+router.get('/pets', async (req, res) => {
+    try {
+        const pets = await Pet.find();
+        res.status(200).json({
+            pets: pets
+        });
+    } 
+    catch(err) {
+        res.status(500).json({
+            msg: 'broken'
+        });
+    }
+});
+
+// get one special et by id
+router.get('/pets/:id', async (req, res) => {
+    const { id } = req.params; 
+    try {
+        const pets = await Pet.find({ _id: id });
+        res.status(200).json({
+            pets: pets
+        });
+    }
+    catch(err) {
+        res.status(500).json({
+            msg: 'broken'
+        });
+    }
+});
+
+// create new pet
+router.post('/pets/', async (req, res) => {
+    const { name, owner, petType, age, createdAt } = req.body;
+    try {
+        const pet = new Pet({ name, owner, petType, age, createdAt });
+        await pet.save();
+        res.status(201).json({
+            msg: 'saved pet',
+            pet
+        });
+    } catch(err){
+        res.status(500).json({
+            msg: 'pet not created'
+        });
+    }
+});
+
+//update pet by id
+router.put('/pets/:id', async (req, res) => {
+    const { id } = req.params;
+    const { name, owner } = req.body;
+    try {
+        const updatedPet = await Pet.findByIdAndUpdate( id, { name, owner }, { new: true });
+        res.status(200).json({
+            msg: 'update successful',
+            pet: updatedPet
+        });
+    } catch (error) {
+        res.status(500).json({
+            msg: 'pet did not update'
+        });
+    }
+});
+
+//delete pet by id
+router.delete('/pets/:id', async (req, res) => {
+    const { id } = req.params; 
+    try {
+        await Pet.findByIdAndRemove(id);
+        res.status(200).json({
+            msg: 'yay deleted!'
+        });
+    }
+    catch(err) {
+            res.status(500).json({
+                msg: 'broken'
+            });
+    }
+});
+
+
+module.exports = router;
